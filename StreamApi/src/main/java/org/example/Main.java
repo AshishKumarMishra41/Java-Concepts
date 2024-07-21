293d40410809261f835da27da7b87d7df46fe9b5
ashishkumarmishra <mishraashishkumar41@gmail.com>
2024-07-21 23:24:28 +0530

    commit

--- StreamApi/src/main/java/org/example/Main.java
+++ StreamApi/src/main/java/org/example/Main.java
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<String> words = Arrays.asList("apple", "banana", "avocado", "blueberry", "apricot");
+
         List<Integer> filteredNum = num.stream().filter(m -> m%2 != 0).map(n -> n*2).collect(Collectors.toList());
         System.out.println(filteredNum);
 
-        List<String> words = Arrays.asList("apple", "banana", "avocado", "blueberry", "apricot");
         List<String> filteredWords = words.stream()
                 .map(String::toUpperCase)
                 .filter(s -> s.startsWith("A"))
@@ -24,9 +25,25 @@ public class Main {
                 reduce(0, Integer::sum);
         System.out.println(sum);
 
+
+        //Collectors concept
         Map<Integer,List<String>> collectedStringsInMap = words.stream()
                 .sorted().
                 collect(Collectors.groupingBy(String::length));
         System.out.println(collectedStringsInMap);
+
+        Map<Integer, Long> collectedStringCountInMap = words.stream()
+                .collect(Collectors.groupingBy(String::length, Collectors.counting()));
+        System.out.println(collectedStringCountInMap);
+
+        Map<Integer, Integer> collectedIntegerGrouping = num.stream()
+                .collect(Collectors.groupingBy(n -> n % 3, Collectors.summingInt(Integer::intValue)));
+        System.out.println(collectedIntegerGrouping);
+
+        Map<Integer, String> collectedStringCustomCollector = words.stream()
+                .collect(Collectors.groupingBy(String::length, Collectors.joining(",")));
+        System.out.println(collectedStringCustomCollector);
+
+
     }
 }
